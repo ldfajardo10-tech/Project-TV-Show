55da27d5f5e76841a6acc612a199a5e6861d9256
ldfajardo10-tech <ld.fajardo10@gmail.com>
2026-01-01 23:33:56 +0100

    episode cards function created

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+const allEpisodes = getAllEpisodes();
+
+function createEpisodeCard(episode) {
+  const episodeCard = document
+    .getElementById("episode-card")
+    .content.cloneNode(true);
+  episodeCard.querySelector("h3").textContent = episode.name;
+  episodeCard.querySelector("p").textContent = episode.season;
+  episodeCard.querySelector("[data-number]").textContent = episode.number;
+  episodeCard.querySelector("[data-summary]").textContent = episode.summary;
+
+
+  return episodeCard;
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+document.body.append(createEpisodeCard(allEpisodes));
+
+const episodeCards = [];
+for (const episode of allEpisodes) {
+  const card = createEpisodeCard(episode);
+  document.body.appendChild(card);
+  episodeCards.push(card);
 }
+
+document.body.append(...episodeCards);
+
 
 window.onload = setup;
