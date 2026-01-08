7224edb7f2f9dcf6fb2777144cfeb2046de98776
ldfajardo10-tech <ld.fajardo10@gmail.com>
2026-01-08 15:18:21 +0100

    refactoring to add state and rendering function + user input

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const allEpisodes = getAllEpisodes();
+const allEpisodes = getAllEpisodes ; //we get the data from all episodes
+const state = {                          //we created a const state with an object with our episodes and with an empty string value to storage the user input.
+  valuesFromFunction : getAllEpisodes(),
+  
+  searchTerm : ""
+};
 
-function createEpisodeCard(episode) {
-  const episodeCard = document
+function createEpisodeCard(episode) { 
+  const episodeCard = document      // we created a const to clone the template from the html file
     .getElementById("episode-card")
     .content.cloneNode(true);
 
-  const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
-  const titleEl = episodeCard.querySelector("[data-name]");
+  const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`; 
+  const titleEl = episodeCard.querySelector("[data-name]"); // we mix the name of the episode with the code episode as a title
   titleEl.textContent = `${episode.name} - ${episodeCode}`; 
-  episodeCard.querySelector("[data-summary]").textContent = episode.summary.replace(/<\/?p>/gi, "");
+  episodeCard.querySelector("[data-summary]").textContent = episode.summary.replace(/<\/?p>/gi, ""); //using replace method and regex to eliminate the <p> tags from the summary
   const img = episodeCard.querySelector("[data-image]");
   img.src = episode.image.medium;
   img.alt = episode.name;
@@ -16,13 +21,30 @@ function createEpisodeCard(episode) {
   return episodeCard;
 }
 
-const container = document.getElementById("episodes-container");
+function render() {
+  // with this function we render the website each time we do a search
 
+  const filteredEpisodes = state.valuesFromFunction.filter(function (episode) {
+    //we filter all the data making sure the input is a string and case insensitive
+    return (
+      String(episode.name).toLowerCase().includes(state.searchTerm) ||
+      String(episode.summary).toLowerCase().includes(state.searchTerm)
+    );
+  });
 
-for (const episode of allEpisodes) {
-  const card = createEpisodeCard(episode);
-  container.appendChild(card);
-}
+  const episodeCards = filteredEpisodes.map(createEpisodeCard); //For every episode that matched the search, create a DOM element (a card) for it
+  document.getElementById("episodes-container").append(...episodeCards);
+};
+
+render(); //render the website with the new DOM elements
+
+const input = document.querySelector("input"); //select the input tag and stores it in the variable input
+input.addEventListener("input", function () { //fires every time the input changes (typing, pasting, deleting text)
+  state.searchTerm = input.value.toLowerCase(); //stores the input value with case-insensitive in the state.searchTerm 
+
+  document.getElementById("episodes-container").innerHTML = ""; //clear the previous list of episodes and render then creates and append the matching episodes
+  render();
+});
 
 
 window.onload = setup;
